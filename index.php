5223818f26b9abddae9c1acecc6449dfbb7be8f6
cooldadpresident <bumabuch@gmail.com>
2024-04-03 15:23:27 +0200

    added php

--- index.php
+++ index.php
@@ -1,43 +1,46 @@
 <?php
-if(isset($_GET['showall']) && $_GET['showall'] == 1){
-    header('application/json: utf-8', true, 200);
-    echo json_encode(
-        [
-'state' = 1, 'content' =>
-'<b>Ahoj</b>' ] ); exit;
+if (isset($_GET['showall']) && $_GET['showall'] == 1) {
+  header('application/json: utf-8', true, 200);
+  echo json_encode(
+    [
+      'state' == 1,
+      'content' => '<b>Ahoj</b>'
+    ]
+  );
+  exit;
 
 }
 ?>
 <html>
-  <head>
-    <script
-      src="https://code.jquery.com/jquery-1.12.4.js"
-      integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
-      crossorigin="anonymous"
-    ></script>
 
-    <script>
-      $(function () {
-        $("#button").on("click", function () {
-          $.ajax({
-            url: "?",
-            type: "GET",
-            data: { showall: 1 },
-          }).done(
-            function (response) {
-              if (response.state) {
-                $("#content".html(response.content));
-              }
-            },
+<head>
+  <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+    crossorigin="anonymous"></script>
 
-            {}
-          );
-        });
+  <script>
+    $(function () {
+      $("#button").on("click", function () {
+        $.ajax({
+          url: "?",
+          type: "GET",
+          data: { showall: 1 },
+        }).done(
+          function (response) {
+            if (response.state) {
+              $("#content".html(response.content));
+            }
+          },
+
+          {}
+        );
       });
-    </script>
-  </head>
-  <body>
-    <button id="button">Get content</button>
-    <div id="content"></div>
-  </body>
+    });
+  </script>
+</head>
+
+<body>
+  <button id="button">Get content</button>
+  <div id="content"></div>
+</body>
+
 </html>
--- request.php
+++ request.php
@@ -1,33 +1,33 @@
 <html>
-  <head>
-    <script
-      src="https://code.jquery.com/jquery-1.12.4.js"
-      integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
-      crossorigin="anonymous"
-    ></script>
 
-    <script>
-      $(function () {
-        $('#button').on('click', function () {
-          $.ajax({
-            url: "?",
-            type: "GET",
-            data: { "showall": 1 },
-          }).done(
-            function (response) {
-              if (response.state) {
-                $("#content".html(response.content));
-              }
-            },
+<head>
+  <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+    crossorigin="anonymous"></script>
 
-            {}
-          );
-        });
+  <script>
+    $(function () {
+      $('#button').on('click', function () {
+        $.ajax({
+          url: "?",
+          type: "GET",
+          data: { "showall": 1 },
+        }).done(
+          function (response) {
+            if (response.state) {
+              $("#content".html(response.content));
+            }
+          },
+
+          {}
+        );
       });
-    </script>
-  </head>
-  <body>
-    <button id="button">Get content</button>
-    <div id="content"></div>
-  </body>
+    });
+  </script>
+</head>
+
+<body>
+  <button id="button">Get content</button>
+  <div id="content"></div>
+</body>
+
 </html>
